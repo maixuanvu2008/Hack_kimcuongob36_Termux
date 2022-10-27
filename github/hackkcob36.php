01a87af430ae1e322b440f6d409bcf884b4e68df
maixuanvu2008 <115379642+maixuanvu2008@users.noreply.github.com>
2022-10-27 16:22:03 +0700

    Add files via upload

---
+++ github/hackkcob36.php
@@ -0,0 +1,121 @@
+<?php
+system("clear");
+$xanhla = "\033[1;32m";
+$res="\033[0m";
+$do="\033[0;31m";
+$grean="\033[1;32m";
+$mit="\033[0;32m";
+$mti="\033[0;33m";
+$test="\033[1;33m";
+$tim="\033[1;36m";
+$lam="\033[1;34m";
+$tmi="\033[1;34m";
+$hong="\033[1;35m";
+$imt="\033[1;35m";
+$blue="\033[1;36m";
+$trang="\033[1;37m";
+$ress="\033[0;32m";
+$res="\033[0;33m";
+$red="\033[0;31m";
+$green="\033[0;37m";
+$yellow="\033[0;33m";
+$white= "\033[0;33m";
+$xnhac= "\033[1;96m";
+$den="\033[1;90m";
+$do="\033[1;91m";
+$luc="\033[1;92m";
+$vang="\033[1;93m";
+$xduong="\033[1;94m";
+$hong="\033[1;95m";
+$trang="\033[1;97m";
+$banner="\r\n";
+echo "$xanhla
+███████╗██████╗░███████╗███████╗███████╗██╗██████╗░███████╗
+██╔════╝██╔══██╗██╔════╝██╔════╝██╔════╝██║██╔══██╗██╔════╝
+█████╗░░██████╔╝█████╗░░█████╗░░█████╗░░██║██████╔╝█████╗░░
+██╔══╝░░██╔══██╗██╔══╝░░██╔══╝░░██╔══╝░░██║██╔══██╗██╔══╝░░
+██║░░░░░██║░░██║███████╗███████╗██║░░░░░██║██║░░██║███████╗
+╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝
+";
+echo "$vang
+
+▀█▀ █▀█ █▀█ █░░   █▄▀ █ █▀▄▀█   █▀▀ █░█ █▀█ █▄░█ █▀▀
+░█░ █▄█ █▄█ █▄▄   █░█ █ █░▀░█   █▄▄ █▄█ █▄█ █░▀█ █▄█
+
+";
+echo "$xanhla ==============================================================";
+echo "
+$hong :$vang Mai Xuân Vũ$hong | GitHub:$vang https://github.com/maixuanvu2008 \n";
+echo "$hong Twitter Lite:$vang @Xuanvuma$hong | Zalo:$vang 0355252638 \n";
+echo "$xanhla ============================================================== \n \n";
+while (true) {
+echo "$xanhla Bạn chọn tính năng nào: \n$vang 1. Hack Kim Cương \n 2. Hack Trang Bị \n 3. Hack Nhân Vật \n";
+echo "$do Bạn chọn số mấy:$vang ";
+$so = trim(fgets(STDIN));
+$so1 = "1";
+if ($so == $so1)
+{
+sleep(2);
+echo "$vang Đang quét tính năng \n";
+sleep (3);
+break;
+} else {
+echo "$xanhla Đang quét tính năng \n";
+sleep (2);
+echo "$vang Tính năng này hiện tại đang được xây dựng";
+sleep (2);
+}
+}
+while (true) {
+echo "$xanhla Vui lòng nhập mật khẩu TOOL: ";
+$pass = trim(fgets(STDIN));
+$pass1 = "hackffob36.1.1";
+if ($pass == $pass1)
+{
+sleep(2);
+echo "$vang Mật khẩu chính xác \n";
+sleep (3);
+break;
+} else {
+echo "$vang Bạn nhập sai rồi \n";
+sleep (2);
+}
+}
+echo "$do Vui Lòng Nhập$vang Mã Mời UID$do Của Bạn Tại Đây: ";
+$id1 = trim(fgets(STDIN));
+sleep(2);
+for ($i=1; $i>=0; $i--) {
+echo "$xanhla Vui lòng chờ$do  $i $xanhla Giây Để Kết Nối Nick Với Tool... \r";
+sleep(1);
+}
+echo "\n";
+echo "$vang Thành công \n \n";
+sleep(1);
+echo "$do ______________________________________\n \n";
+sleep (1);
+system ('clear');
+echo "$xanhla
+███████╗██████╗░███████╗███████╗███████╗██╗██████╗░███████╗
+██╔════╝██╔══██╗██╔════╝██╔════╝██╔════╝██║██╔══██╗██╔════╝
+█████╗░░██████╔╝█████╗░░█████╗░░█████╗░░██║██████╔╝█████╗░░
+██╔══╝░░██╔══██╗██╔══╝░░██╔══╝░░██╔══╝░░██║██╔══██╗██╔══╝░░
+██║░░░░░██║░░██║███████╗███████╗██║░░░░░██║██║░░██║███████╗
+╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝
+";
+echo "$vang
+
+▀█▀ █▀█ █▀█ █░░   █▄▀ █ █▀▄▀█   █▀▀ █░█ █▀█ █▄░█ █▀▀
+░█░ █▄█ █▄█ █▄▄   █░█ █ █░▀░█   █▄▄ █▄█ █▄█ █░▀█ █▄█
+
+";
+echo "$xanhla ==============================================================";
+echo "
+$hong :$vang Mai Xuân Vũ$hong | GitHub:$vang https://github.com/maixuanvu2008 \n";
+echo "$hong Twitter Lite:$vang @Xuanvuma$hong | Zalo:$vang 0355252638 \n";
+echo "$xanhla ============================================================== \n \n";
+while (true) {
+echo "$xanhla Bạn đã nhận$vang ";
+echo(rand(500,1500));
+echo "$blue Kim Cương \n";
+sleep (1);
+}
